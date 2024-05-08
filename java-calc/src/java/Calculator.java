39a5bb04fc5fe6e296d644bfad13d1bafa0c5a7d
iren125 <168932034+iren125@users.noreply.github.com>
2024-05-08 17:34:40 +0300

    Add files via upload
    2.0

--- java-calc/src/java/Calculator.java
+++ java-calc/src/java/Calculator.java
@@ -4,59 +4,70 @@ public class Calculator {
     public static void main(String[] args) {
 
         Converter converter = new Converter();
-        String[] actions = {"+", "-", "/", "\\*"};
+        String[] actions = {"+", "-", "/", "*"};
         String[] regexActions = {"\\+", "-", "/", "\\*"};
         Scanner scn = new Scanner(System.in);
         String exp = scn.nextLine();
 
-        int actionIndex=-1;
+        int actionIndex = -1;
         for (int i = 0; i < actions.length; i++) {
-            if(exp.contains(actions[i])){
+            if (exp.contains(actions[i])) {
                 actionIndex = i;
                 break;
-
             }
         }
 
-        if(actionIndex==-1){
+        if (actionIndex == -1) {
             System.out.println("неккоректное выражение");
-        return;
+            return;
         }
 
         String[] data = exp.split(regexActions[actionIndex]);
+        if (data.length != 2) {
+            System.out.println("неккоректное выражение");
+            return;
+        }
 
-        if(converter.isRoman(data[0]) == converter.isRoman(data[1])){
-            int a,b;
+        if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
+            int a, b;
 
             boolean isRoman = converter.isRoman(data[0]);
-            if(isRoman){
+            if (isRoman) {
                 a = converter.romanToInt(data[0]);
                 b = converter.romanToInt(data[1]);
-
-            }else{
+            } else {
                 a = Integer.parseInt(data[0]);
                 b = Integer.parseInt(data[1]);
             }
+
+            if (b == 0 && actions[actionIndex].equals("/")) {
+                System.out.println("Деление на ноль невозможно");
+                return;
+            }
+
             int result;
-            switch (actions[actionIndex]){
+            switch (actions[actionIndex]) {
                 case "+":
-                    result = a+b;
+                    result = a + b;
                     break;
                 case "-":
-                    result = a-b;
-               break;
-               case "*":
-                result = a*b;
-               break;
-               default:
-                result = a/b;
-               break;
-             }
-             if(isRoman){
-                 System.out.println(converter.intToRoman(result));
-} else{
-                 System.out.println(result);             }
-}else{
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                default:
+                    result = a / b;
+                    break;
+            }
+
+            if (isRoman) {
+                System.out.println(converter.intToRoman(result));
+            } else {
+                System.out.println(result);
+            }
+        } else {
             System.out.println("Числа должны быть в одном формате");
         }
+    }
 }
